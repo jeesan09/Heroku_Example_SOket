31102b10a701c21034fe3f02eeb25feed9d9eaba
jeesan09 <jeesan@eatlbd.com>
2018-09-04 12:29:29 +0600

    doneMayBe

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ io.on('connection',function(socket){
 
      socket.on('chat', function(data){
         // console.log(data);
-        io.sockets.emit('chat', data);
+        io.sockets.emit('chat', data,3000);
     });
      
     // Handle typing event
     socket.on('typing', function(data){
-        socket.broadcast.emit('typing', data);
+        socket.broadcast.emit('typing', data,3000);
     });
 
 
